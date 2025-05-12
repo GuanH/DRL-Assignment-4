23109414ab20f476c32be834a5e523995f76d1be
GuanH <r13922167@ntu.edu.tw>
2025-05-12 17:28:35 +0800

    cpu mode

--- Q3/student_agent.py
+++ Q3/student_agent.py
@@ -29,7 +29,7 @@ class Actor(nn.Module):
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
-        self.device = torch.device("cuda")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.action_space = gym.spaces.Box(-1.0, 1.0, (21,), np.float64)
         self.actor = Actor().to(self.device)
         self.actor.load_state_dict(torch.load('model.pth'), map_location=self.device)
